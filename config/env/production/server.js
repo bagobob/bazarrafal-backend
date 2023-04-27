7e996b3dcf3e0957a7a2023a501a51cea42da023
bagobob <htibago@gmail.com>
2023-04-27 20:47:10 +0200

    pusblish backend

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,15 +1,10 @@
-
 module.exports = ({ env }) => ({
-  proxy: true,
-  host: "0.0.0.0",
-  port: process.env.PORT,
-  url: env('MY_HEROKU_URL'),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS')
+    keys: env.array('APP_KEYS'),
   },
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
-    },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-})
+});
